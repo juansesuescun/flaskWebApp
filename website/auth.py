1f5cd0b61e7f45a5f3beb1168afc658b12f1d148
juansesuescun <75804420+juansesuescun@users.noreply.github.com>
2023-07-29 11:13:29 -0500

    configuracion de flash messages en auth.py y muestra de menssages con button en base.html

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 # definicion de blueprint para autenticacion o login
 auth = Blueprint('auth', __name__)
 
 
 #Definiendo las rutas para login logout
-@auth.route('/login')
+@auth.route('/login', methods = ['GET', 'POST'])
 def login():
     return render_template("login.html", boolean = True)
 
@@ -13,6 +13,23 @@ def login():
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods = ['GET', 'POST'])
 def sign_up():
+    if request.method =='POST':
+        email = request.form.get('email')
+        firstname = request.form.get('firstname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email)< 4:
+            flash('Email must be greater than 3 characters.', category = 'error')
+        elif len(firstname) <2:
+            flash('First Name must be greater than 1 character.', category = 'error')
+        elif password1 != password2:
+            flash('password don\'t match.', category = 'error')
+        elif len(password1) <7:
+            flash('password must be at least 7 characters.', category = 'error')
+        else:
+            # add user to database
+            flash('account created', category='success')
     return render_template("sign_up.html")
